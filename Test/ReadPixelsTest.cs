1b61a80879eb5c9abee86407a04e3916bd85592b
Yves Wang <paraself@gmail.com>
2015-09-22 22:44:00 +0800

    - test if batch reading from GPU will increase performance, and the result is yes.

--- Test/ReadPixelsTest.cs
+++ Test/ReadPixelsTest.cs
@@ -1,29 +1,48 @@
-﻿using UnityEngine;
+﻿/// <summary>
+/// 我刚才试了下 一个2048 * 2048 的材质，从GPU读取到CPU 每帧耗时 50ms
+/// 而 如果把这个2048 * 2048 的材质，划分成 4 * 4 = 16 个小材质的话，分块儿读，一帧耗时 90ms
+/// 看来 放在一起读取，还是会快
+/// 所以 我可以不把计算batch 在一起，仅仅把读取Batch了
+/// </summary>
+
+
+using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
 
 public class ReadPixelsTest : MonoBehaviour {
 
 	public RenderTexture rt;
 	public RenderTexture[] rtt;
-	public Texture2D t2d;
+	public Texture2D t2d,t2dt;
 	private Shader sd;
 	private Material mtl;
 	private Camera cam;
 	int head = 0;
+	
+	Stopwatch s;
+	
+	float time = 60f;
 
 	// Use this for initialization
 	void Start () {
-		rt = new RenderTexture (Screen.width,Screen.height,0,RenderTextureFormat.Default);
-		rtt = new RenderTexture[5] ;
-		for (int i=0;i<5;i++) {
-			rtt[i] = new RenderTexture (Screen.width * 2,Screen.height * 2,0,RenderTextureFormat.Default);
+	
+		s = new Stopwatch ();
+		
+		rt = new RenderTexture (2048,2048,0,RenderTextureFormat.Default);
+		rt.Create();
+		
+		rtt = new RenderTexture[16] ;
+		for (int i=0;i<16;i++) {
+			rtt[i] = new RenderTexture (2048 /4,2048/4,0,RenderTextureFormat.Default);
 			rtt[i].Create();
 		}
 		
-		t2d = new Texture2D (Screen.width * 2,Screen.height * 2,TextureFormat.ARGB32,false,false);
-		rt.Create();
+		t2d = new Texture2D (2048,2048,TextureFormat.RGBAFloat,false,false);
+		t2dt = new Texture2D (2048/4,2048/4,TextureFormat.RGBAFloat,false,false);
+		
 		sd = Shader.Find("Sprites/Default");
-		Debug.Log(sd);
+		
 		mtl = new Material (sd);
 		mtl.SetColor("_Color",Color.red);
 		cam = Camera.main;
@@ -31,36 +50,39 @@ public class ReadPixelsTest : MonoBehaviour {
 		cam.targetTexture = rt;
 		
 		cam.Render();
-		Graphics.Blit(rt,rtt[head],mtl);
+		//Graphics.Blit(rt,rtt[head],mtl);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		cam.Render();
-		//head++;
-		//head %= 5;
-		rtt[head].DiscardContents();
-		Graphics.Blit(rt,rtt[head],mtl);
+		//cam.Render();
+		
 		StartCoroutine(ReadPixels());
 
 	}
 	
 	void OnRenderObject2() {
-		//int th = head +1;
-		//if (th==5) th = 0;
-		int th = 0;
-		RenderTexture.active = rtt[th];
-		t2d.ReadPixels(new Rect(0, 0, rtt[th].width, rtt[th].height), 0, 0);
-		
-		rtt[th].DiscardContents();
+		RenderTexture.active = rt;
+		t2d.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+		rt.DiscardContents();
 		t2d.Apply();
 		RenderTexture.active = null;//you must set it back to null, otherwise unity crashes.
 	}
 	
 	IEnumerator ReadPixels() {
 		yield return new WaitForEndOfFrame();
-		OnRenderObject2();
+		Test();
+	}
+	
+	void Test () {
+		for (int i=0;i<16;i++) {
+			RenderTexture.active = rtt[i];
+			t2dt.ReadPixels(new Rect(0, 0, rtt[i].width, rtt[i].height), 0, 0);
+			rtt[i].DiscardContents();
+			t2dt.Apply();
+			RenderTexture.active = null;
+		}
 	}
 	
 	
